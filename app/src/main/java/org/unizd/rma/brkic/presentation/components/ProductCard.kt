5ac55666298de97a5e932f348e560145b010c5a4
anamarija33 <sybile@proton.me>
2026-09-05 22:01:06 +0200

    created 3 components: empty screen, product list and product card

---
+++ app/src/main/java/org/unizd/rma/brkic/presentation/components/ProductCard.kt
@@ -0,0 +1,112 @@
+package org.unizd.rma.brkic.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.unizd.rma.brkic.domain.models.SkincareItem
+import coil.compose.AsyncImage
+import androidx.compose.material.icons.Icons
+import androidx.room3.Delete
+
+@Composable
+fun ProductCard(
+    product: SkincareItem,
+    onProductClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onProductClick() }
+            .padding(horizontal = 8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Placeholder slika
+            if (product.imageUri != null) {
+                AsyncImage(
+                    model = product.imageUri,
+                    contentDescription = product.name,
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                Surface (
+                    modifier = Modifier
+                        .size(50.dp)
+                        .padding(end = 16.dp),
+                    shape = MaterialTheme.shapes.medium,
+                    color = MaterialTheme.colorScheme.primaryContainer
+                ) {
+                    Box (
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = product.name.first().toString(),
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+            }
+
+
+            // Info
+            Column (
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = product.name,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = product.brand,
+                    fontSize = 12.sp,
+                    color = MaterialTheme.colorScheme.outline
+                )
+            }
+
+            // Delete
+            IconButton(
+                onClick = onDeleteClick
+            ) {
+                Icon(
+                    Icons.Filled.Delete,
+                    contentDescription = "Obriši",
+                    tint = MaterialTheme.colorScheme.error
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/unizd/rma/brkic/presentation/components/ProductList.kt
@@ -0,0 +1,32 @@
+package org.unizd.rma.brkic.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.unizd.rma.brkic.domain.models.SkincareItem
+
+@Composable
+fun ProductList (
+    products: List<SkincareItem>,
+    onProductClick: (SkincareItem) -> Unit,
+    onDeleteClick: (SkincareItem) -> Unit
+) {
+    LazyColumn (
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(products, key = { it.id }) { product ->
+            ProductCard(
+                product,
+                onContactClick = { onProductClick(product) },
+                onDeleteClick = { onDeleteClick(product) }
+            )
+        }
+    }
+}
